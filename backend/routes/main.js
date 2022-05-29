c307935bf57a40266adc9c79c7ec8baf626a574f
jfilippin <jarod.fillippin@gmail.com>
2022-05-29 15:49:02 -0300

    v0.3.1

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
+const NW = require('../functions/needleman-wunsch');
 
 router.post('/', (req, res) => {
-    var seq1 = JSON.stringify(req.body.data.seq1);
-    var seq2 = JSON.stringify(req.body.data.seq2);
+    var seq1 = JSON.stringify(req.body.data.seq1).slice(1).slice(0, -1);
+    var seq2 = JSON.stringify(req.body.data.seq2).slice(1).slice(0, -1);
     
-    console.log("Sequence 1: " + seq1);
-    console.log("Sequence 2: " + seq2);
+    console.log(NW(seq1, seq2));
 
     res.status("200");
     return res.send("ok");
